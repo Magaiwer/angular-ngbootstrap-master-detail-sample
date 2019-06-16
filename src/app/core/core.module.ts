4f33533c064e4ca4fd9f0b81f8cbf2f202d53719
Magaiver Santos <magaiver@tricon.inf.br>
2019-06-16 13:21:10 -0300

    improving navbarComponent

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,29 @@
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NavBarComponent } from '../core/nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [NavBarComponent],
   imports: [
     CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    RouterModule
+   // HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
+  ],
+  exports: [
+    //shared modules
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
 
+    //Shared components
     NavBarComponent
   ]
 })
